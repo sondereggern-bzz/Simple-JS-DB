2527de581edcd8ffeaab940e10d542161ac9f038
sondereggern@bzz.ch <sondereggern@bzz.ch>
2024-01-09 08:57:20 +0100

    v0.3

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 // datenbank.js
 
 var automodelle = [
-    { id: 1, marke: "Audi", modell: "A7", baujahr: 2020 },
-    { id: 2, marke: "Audi", modell: "RS6", baujahr: 2021 },
-    { id: 3, marke: "Porsche", modell: "911", baujahr: 2021 },
-    { id: 4, marke: "Audi", modell: "Modell3", baujahr: 2022 },
-    { id: 5, marke: "Audi", modell: "A7", baujahr: 2019 },
+    { id: 1, marke: "Audi", modell: "A7", baujahr: 2020, motor: "3L V6 TFSI", leistung: 340},
+    { id: 2, marke: "Audi", modell: "RS6", baujahr: 2021, motor: "4L V8", leistung: 600 },
+    { id: 3, marke: "Porsche", modell: "911", baujahr: 2021, motor: "3L F6", leistung: 450 },
+    { id: 4, marke: "Audi", modell: "s5", baujahr: 2022, motor: "3L V6 TFSI", leistung: 350 },
+    { id: 5, marke: "Volvo", modell: "S60", baujahr: 2021, motor: "2L i4 ", leistung: 250 },
+    { id: 6, marke: "Maserati", modell: "Ghibli", baujahr: 2023, motor: "3.7L V8", leistung: 580 },
 ];
 function fillDropdown() {
     var dropdown = document.getElementById('modellAuswahl');
